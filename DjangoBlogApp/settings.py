efcda0e8209174c462e45983144f54d6d84532f2
TylerDev16 <97607018+TylerDev16@users.noreply.github.com>
2022-07-20 08:46:59 -0400

    heroku deployment

--- DjangoBlogApp/settings.py
+++ DjangoBlogApp/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cyd560t=2skj*lo7=(svjkeieyp)zu(t^z@^d9nsfnvmw@^=yer&@#^5)r4^3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', 'djangoblogtyler.herokuapp.com']
 
